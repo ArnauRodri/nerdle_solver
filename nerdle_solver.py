6d4d33ae0da68659727b41b79a9177cc33472dea
ArnauRodri <35958499+ArnauRodri@users.noreply.github.com>
2023-04-19 12:12:10 +0200

    Minor change

--- nerdle_solver.py
+++ nerdle_solver.py
@@ -1,5 +1,5 @@
 # This file is part of the nerdle_solver distribution
-# (https://github.com/nerdle_solver or http://nerdle_solver.github.io).
+# (https://github.com/ArnauRodri/nerdle_solver.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
